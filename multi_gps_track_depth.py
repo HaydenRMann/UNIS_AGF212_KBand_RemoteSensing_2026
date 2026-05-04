96a301107b4fb6c69490bd93c406213c5fa99daa
Hayden Mann <165971854+HaydenRMann@users.noreply.github.com>
2026-05-04 20:41:28 +0200

    Update multi_gps_track_depth.py

--- multi_gps_track_depth.py
+++ multi_gps_track_depth.py
@@ -3,27 +3,30 @@ Author: Hayden Mann
 
 Description: Plots and maps snow depth from the radar transect data. 
 Requires GPS input.
-
 """
+
+
+###
+# IMPORTS
+###
 
 from radar import ReadFile, SkyCalibration, Combine_channels
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import geopandas as gpd
-
 import matplotlib.ticker as mticker
 import cartopy.mpl.ticker as cticker
-
+import matplotlib.colors as mcolors
+import pyproj
 
 
 """
-Goals:
+Objectives:
 
 1. Get gps data and corresponding time (slowtime)
 2. Get snow thickness and corresponding slowtime
@@ -32,6 +35,10 @@ Goals:
 5. Turn into map
 """
 
+
+###
+# TRANSECT DIRECTORY 
+###
 """
 MOVING:
 
@@ -39,34 +46,49 @@ MOVING:
 10/03 - 3, 5, 6, 7, 8
 """
 
+
+###
+# PLACE STAND-ALONE TRANSECTS FILENAMES HERE
+###
+
 filenames_moving = ["10_03_2026_3", "10_03_2026_5", "10_03_2026_6", "10_03_2026_7", "10_03_2026_8"]
 # filenames_moving = ["09_03_2026_7", "10_03_2026_5", "10_03_2026_7", "10_03_2026_8"]
 
 
-# filename = "10_03_2026_7"
+
+###
+# Starting Data Processing
+###
+
+# Instantiate arrays for plotting
 lats = []
 lons = []
 z = []
 
+# IMPORT Sky-calibrate filename
 FILENAME_SKY_CALIBRATION = "09_03_2026_1"
+
+# Loop through all desired filenames
 for filename in filenames_moving:
+
+    # Read filename to get snow thickness
     inp = pd.read_csv('Radar_DATAAA/' + filename + '_pick_vars.csv')  # now it's a DataFrame
-    snow_thickness = inp['snow_thickness']  # * -1
+    snow_thickness = inp['snow_thickness']  
     slowtime_thickness = inp['slowtime']
 
+    # Read/calibrate to receive time and gps. Timestamps match with snow thickness
     SKY_CALIBRATION = ReadFile(FILENAME_SKY_CALIBRATION)
     radar_data = SkyCalibration(SKY_CALIBRATION, ReadFile(filename))
     radar_data = Combine_channels(radar_data)
     slowtime_radar = radar_data['slowtime']
     gps_data = radar_data['gps']
 
-
-
+    # Temporary arrays
     file_lats = []
     file_lons = []
     file_time_gps_array = []
-    # snow_thickness = []
 
+    # Extract GPs
     for loc in gps_data:
         try:
             file_lats.append(loc.latitude)
@@ -78,28 +100,27 @@ for filename in filenames_moving:
     file_latslats = np.array(file_lats)
     file_lonslons = np.array(file_lons)
 
+    # Extend arrays (keeps indices consistent)
     lats.extend(file_latslats)
     lons.extend(file_lonslons)
     z.extend(snow_thickness.values)
 
+# Final arrays
 lats = np.array(lats)
 lons = np.array(lons)
 z = np.array(z)
-# x = lons
-# y = lats
-# z = snow_thickness.values  
-# x = lats
 
 
-
-"""
-AVERAGE OF OUR 3 on 10.03.2026_4
-"""
+###
+# IF YOU HAVE OVERLAPPING TRANSECTS: if not, you can comment out
+# this section takes the mean when multiple people have picked data from the same transect, 
+# in order to save time/CPU on interpolation and avoid linear algebraic errors.
+###
 overlapped_lats = []
 overlapped_lons = []
 overlapped_z = []
 
-
+# Same logic as above section, just taking the mean instead of extending. 
 filenames_moving = ["10_03_2026_4_pick_vars_hayden.csv", "10_03_2026_4_pick_vars_niklas.csv", "10_03_2026_4_pick_vars_Carla.csv", "10_03_2026_4_pick_vars_chloe.csv"]
 filename_og_04 = "10_03_2026_4"
 for filename in filenames_moving:
@@ -112,8 +133,6 @@ for filename in filenames_moving:
     radar_data = Combine_channels(radar_data)
     slowtime_radar = radar_data['slowtime']
     gps_data = radar_data['gps']
-
-
 
     file_lats = []
     file_lons = []
@@ -136,10 +155,7 @@ for filename in filenames_moving:
     overlapped_z.append(snow_thickness.values)
     print(len(snow_thickness))
 
-# overlapped_lats_correct = np.mean(overlapped_lats, axis=0)
-# overlapped_lons_correct = np.mean(overlapped_lons, axis=0)
-# overlapped_z_correct = np.mean(overlapped_z, axis=0)
-
+# Format arrays
 overlapped_lats_arr = np.stack(overlapped_lats)
 overlapped_lons_arr = np.stack(overlapped_lons)
 overlapped_z_arr   = np.stack(overlapped_z)
@@ -147,20 +163,17 @@ overlapped_z_arr   = np.stack(overlapped_z)
 overlapped_lats_correct = np.mean(overlapped_lats_arr, axis=0)
 overlapped_lons_correct = np.mean(overlapped_lons_arr, axis=0)
 overlapped_z_correct    = np.mean(overlapped_z_arr, axis=0)
-# lats.concatenate(overlapped_lats_correct)
-# lons.concatenate(overlapped_lons_correct)
-# z.concatenate(overlapped_z_correct)
 
 lats = np.concatenate([lats, overlapped_lats_correct])
 lons = np.concatenate([lons, overlapped_lons_correct])
 z = np.concatenate([z, overlapped_z_correct])
 
 
-print(len(z))
+#print(len(z))
 
-"""
-SCATTER
-"""
+###
+# Scatter Map Plot
+###
 
 # ESRI World Imagery tiles (real satellite)
 class ESRIImagery(cimgt.GoogleTiles):
@@ -172,41 +185,27 @@ class ESRIImagery(cimgt.GoogleTiles):
         ).format(z=z, y=y, x=x)
 image = ESRIImagery()
 
+# Set figure size
 # fig = plt.figure(figsize=(8, 8))
 fig = plt.figure(figsize=(8, 6))
-
 ax = plt.axes(projection=image.crs)
 
-# PlateCarree for GPS coords
-"""
-getting interpolation extent:
-"""
+# Retreive interpolation extent
 tellbreen_extent = gpd.read_file("tellbreen_extent_converted.shp")
-TARGET_CRS = "epsg:32633" # 3143 is for north but also rotates it (need to learn projection specificts). apparently 32633 also uses meters only so won't distorr
-#       doesnt affect plots at all, just for extent
+
+# 3143 is for north but also rotates it (need to learn projection specificts). apparently 32633 also uses meters only so won't distorr
+# doesnt affect plots at all, just for extent
+TARGET_CRS = "epsg:32633" 
+
 #  convert tellbreen extent shapefile to target crs
 tellbreen_extent = tellbreen_extent.to_crs(TARGET_CRS)
-
-
 s_minx, s_miny, s_maxx, s_maxy = tellbreen_extent.total_bounds
-
 ax.set_extent(
     [s_minx - 250, s_maxx + 250, s_miny - 250, s_maxy + 250],
     crs=ccrs.UTM(33)
 )
 
-
-# ax.set_extent(
-#     [lons.min()-0.0075, lons.max()+0.0075,
-#      lats.min()-0.0025, lats.max()+0.0025],
-#     crs=ccrs.PlateCarree()
-# )
-
-
 ax.add_image(image, 14) 
-import matplotlib.colors as mcolors
-
-# cmap_rb = mcolors.LinearSegmentedColormap.from_list("red_blue", ["red", "blue"])
 
 sc = ax.scatter(
     lons,
@@ -221,12 +220,7 @@ sc = ax.scatter(
 ax.set_xlabel("Longitude")
 ax.set_ylabel("Latitude")
 
-
-# plt.title("Moving Snow Thickness Measurements: GPS Track on Satellite Map (ESRI)")
-
-
-
-## SCATTER TICKS
+# SCATTER TICKS
 import cartopy.crs as ccrs
 import matplotlib.ticker as mticker
 import cartopy.mpl.ticker as cticker
@@ -247,23 +241,16 @@ gl.bottom_labels = True
 # gl.xlines = False
 # gl.ylines = False
 
-# formattingg
+# Formatting
 gl.xformatter = cticker.LongitudeFormatter()
 gl.yformatter = cticker.LatitudeFormatter()
 
-# how many ticks
 gl.xlocator = mticker.MaxNLocator(nbins=5)
 gl.ylocator = mticker.MaxNLocator(nbins=5)
 
 gl.xlabel_style = {'size': 10}
 gl.ylabel_style = {'size': 10}
 
-
-# divider = make_axes_locatable(ax)
-# cax = divider.append_axes("right", size="5%", pad=0.05)
-   
-# plt.colorbar(sc, label="Snow Thickness")
-# plt.colorbar(sc, ax=ax, orientation='vertical', pad=0.08, shrink=0.8, label="Snow Thickness")
 plt.colorbar(
     sc,
     ax=ax,
@@ -273,21 +260,19 @@ plt.colorbar(
     label="Snow Thickness [m]"
 )
 # sc.set_clim(0, 2)
-sc.set_clim(0.36, 1.65)
+# sc.set_clim(0.36, 1.65) # Can hard-code or not hard-code.
 
-print(z.min())
-print(z.max())
+# print(z.min())
+# print(z.max())
 plt.savefig('latest_transects.png', dpi=600)
-# plt.show()
 
 
 
 
-"""
-Save Data Points
-"""
 
-
+###
+# Save Data Points
+###
 
 df_out = pd.DataFrame({
     "latitude": lats,
@@ -303,123 +288,11 @@ print(np.mean(z))
 
 
 
-
-
-
-# # # Create a figure with two subplots side-by-side
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6), sharey=True)
-
-# ax1.scatter(lats, z, alpha=0.6, color='blue', s=10, edgecolors='none')
-# ax1.set_xlabel('Latitude')
-# ax1.set_ylabel('Snow Thickness [m]')
-# ax1.set_title('Snow Thickness vs. Latitude')
-# # ax1.xformatter = cticker.LatitudeFormatter()
-# ax1.grid(True, linestyle='--', alpha=0.5)
-
-
-# # trend/regression
-# m, b = np.polyfit(lats, z, 1)
-# ax1.plot(lats, m*lats + b, color='red', label=f'Trend: y={m:.2f}x+{b:.2f}')
-# ax1.legend()
-
-
-
-
-# # Plot 2: Snow Thickness vs Longitude
-# ax2.scatter(lons, z, alpha=0.6, color='red', s=10, edgecolors='none')
-# ax2.set_xlabel('Longitude')
-# ax2.set_title('Snow Thickness vs. Longitude')
-# ax2.grid(True, linestyle='--', alpha=0.5)
-# # ax2.xformatter = cticker.LongitudeFormatter()
-
-
-
-
-
-# # fixing grids
-
-
-
-# from matplotlib.ticker import FormatStrFormatter
-
-# ax1.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-# ax2.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-
-
-# plt.tight_layout()
-
-# # Save the plots
-# plt.savefig('thickness_vs_coords.png', dpi=300)
-# plt.show()
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from scipy import stats
-# from matplotlib.ticker import FormatStrFormatter
-
-# # Assuming lats, lons, and z are already defined as numpy arrays
-# # Creating a subplot figure
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-
-# # --- Plot 1: Snow Thickness vs. Latitude ---
-# ax1.scatter(lats, z, alpha=0.6, color='blue', s=10, edgecolors='none')
-
-# # Linear Regression for Latitude
-# slope_lat, intercept_lat, r_lat, p_lat, std_err_lat = stats.linregress(lats, z)
-# line_lat = slope_lat * lats + intercept_lat
-
-# ax1.plot(lats, line_lat, color='darkblue', linewidth=2, 
-#          label=f'Trend: y={slope_lat:.4f}x + {intercept_lat:.2f}\n$R^2$={r_lat**2:.3f}, $p$={p_lat:.3e}')
-
-# ax1.set_xlabel('Latitude')
-# ax1.set_ylabel('Snow Thickness [m]')
-# ax1.set_title('Snow Thickness vs. Latitude')
-# ax1.grid(True, linestyle='--', alpha=0.5)
-# ax1.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-# ax1.legend(fontsize='small')
-
-# # --- Plot 2: Snow Thickness vs. Longitude ---
-# ax2.scatter(lons, z, alpha=0.6, color='red', s=10, edgecolors='none')
-
-# # Linear Regression for Longitude
-# slope_lon, intercept_lon, r_lon, p_lon, std_err_lon = stats.linregress(lons, z)
-# line_lon = slope_lon * lons + intercept_lon
-
-# ax2.plot(lons, line_lon, color='darkred', linewidth=2, 
-#          label=f'Trend: y={slope_lon:.4f}x + {intercept_lon:.2f}\n$R^2$={r_lon**2:.3f}, $p$={p_lon:.3e}')
-
-# ax2.set_xlabel('Longitude')
-# ax2.set_title('Snow Thickness vs. Longitude')
-# ax2.grid(True, linestyle='--', alpha=0.5)
-# ax2.xaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-# ax2.legend(fontsize='small')
-
-# plt.show()
-
-# # # [Image of linear regression]
-
-
-# # plt.tight_layout()
-# # plt.savefig('thickness_vs_coords.png', dpi=300)
-# # plt.show()
-
-
-
 """
-REGRESSION N, E
+Linear Trend: Longitudinal
 """
 
 # convert from lat/lon into x/y
-import pyproj
-proj = pyproj.Transformer.from_crs("epsg:4326", "epsg:32633", always_xy=True)
-x, y = proj.transform(lons, lats)
-
-# find centers
-# x0 = x - x.mean()
-# y0 = y - y.mean()
-
-
-
 proj = pyproj.Transformer.from_crs("epsg:4326", "epsg:32633", always_xy=True)
 x, y = proj.transform(lons, lats)
 
@@ -432,7 +305,7 @@ y0_km = y0 / 1000
 # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 (fig, ax1) = plt.subplots(1, 1, figsize=(12,5))
 
-# ---------------- EAST-WEST ----------------
+# Plot East-West 
 sc = ax1.scatter(x0_km,
              z, 
              s=10, 
@@ -458,7 +331,7 @@ plt.savefig('longitudinal.png', dpi=600)
 plt.show()
 
 
-# ---------------- NORTH-SOUTH ----------------
+# Plot North-South 
 # ax2.scatter(y0_km, z, s=10, alpha=0.5, color='red')
 
 # m2, b2 = np.polyfit(y0_km, z, 1)
